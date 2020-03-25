7a6097d87d501b6b7f68e6468c3fa93d5c6bb972
George Dietrich <george@dietrich.app>
2020-03-25 12:36:14 -0400

    yield the value in the not blank validator

--- src/constraints/not_blank_validator.cr
+++ src/constraints/not_blank_validator.cr
@@ -1,32 +1,33 @@
 struct Athena::Validator::Constraints::NotBlankValidator < Athena::Validator::ConstraintValidator
   def validate(value : String?, constraint : AVD::Constraints::NotBlank) : Nil
-    value = (v = value) && (normalizer = constraint.normalizer) ? normalizer.call(v) : value
+    value = (va = value) && (normalizer = constraint.normalizer) ? normalizer.call(va) : value
 
-    validate(value, constraint) do
-      value.nil? || value.blank?
+    validate(value, constraint) do |v|
+      v.blank?
     end
   end
 
   def validate(value : Bool?, constraint : AVD::Constraints::NotBlank) : Nil
-    validate(value, constraint) do
-      value.nil? || value == false
+    validate(value, constraint) do |v|
+      v == false
     end
   end
 
   def validate(value : Indexable?, constraint : AVD::Constraints::NotBlank) : Nil
-    validate(value, constraint) do
-      value.nil? || value.empty?
+    validate(value, constraint) do |v|
+      v.empty?
     end
   end
 
   private def validate(value : _, constraint : AVD::Constraints::NotBlank, & : -> Bool) : Nil
     return if value.nil? && constraint.allow_nil?
-    return unless yield
 
-    self.context
-      .build_violation(constraint.message)
-      .add_parameter("{{ value }}", value.to_s)
-      .code(AVD::Constraints::NotBlank::IS_BLANK_ERROR)
-      .add
+    if value.nil? || yield value
+      self.context
+        .build_violation(constraint.message)
+        .add_parameter("{{ value }}", value.to_s)
+        .code(AVD::Constraints::NotBlank::IS_BLANK_ERROR)
+        .add
+    end
   end
 end
